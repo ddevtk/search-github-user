a95a6b9b95f72917dd41f530c17f01a5b6152de1
duongbui123 <buiducduong2342000@gmail.com>
2021-07-02 01:03:05 +0700

    setup router , login and error page,

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Switch>
+      <Route exact path='/'>
+        <Dashboard></Dashboard>
+      </Route>
+      <Route path='/login'>
+        <Login />
+      </Route>
+      <Route path='*'>
+        <Error />
+      </Route>
+    </Switch>
   );
 }
 
